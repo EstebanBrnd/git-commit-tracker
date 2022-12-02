1fd1c4b0b588d06acefee868878dee383b01316b
EstebanBrnd <esteban.barneaud@orange.fr>
2022-12-02 23:23:08 +0100

    Ceci est une nouvelle version des fichiers du commit-tracker

--- commit_tracker.py
+++ commit_tracker.py
@@ -1,5 +1,4 @@
 from discord_webhook import DiscordWebhook, DiscordEmbed
-import subprocess
 def get_last():
     with open("last.commit","r") as f:
         return f.read()
@@ -16,6 +15,34 @@ def is_something_new(l,last):
 def get_pos_last(l,last):
     return l.index(last)
 
+def commit_analyse():
+    with open("sortie.txt","r")as f:
+        with open("commit.txt","w")as out:
+            l=f.read().split("\n")
+            list_files=[]
+            for line in l:
+                line=line.split(" ")
+                if len(line)!=1:
+                    if list_files!=[]:
+                        text=" ".join(list_files)
+                        out.write(text)
+                        out.write("\n")
+                        list_files=[]
+                    out.write(line[0])
+                    out.write(" ")
+                if len(line)==1:
+                    list_files.append(line[0])
+            text=" ".join(list_files)
+            out.write(text)
+    with open("commit.txt","r") as f:
+        l=f.read().split("\n")
+        i=0
+        for elt in l:
+            a=elt.split(" ")
+            l[i]=[a[0],"\n".join(a[1:])]
+            i+=1
+    return l
+
 up=get_update()
 last=get_last()
 if is_something_new(up,last):
@@ -23,15 +50,24 @@ if is_something_new(up,last):
         com=f.read().split("\n")
     with open("author.txt","r") as f:
         auth=f.read().split("\n")
+    with open("name.txt","r") as f:
+        name=f.read().strip("\r\n")
+    with open("hash.txt","r") as f:
+        hash=f.read().split("\n")
+    d=commit_analyse()
     i=get_pos_last(up,last)-1
     while i>=0:
         print(f"COMMIT à {up[i]} de {auth[i]} : {com[i]}")
-        webhook = DiscordWebhook(url='EMBED URL')
+        webhook = DiscordWebhook(url='https://discord.com/api/webhooks/1047957113357484032/F9GDQcQ7UnyJlQsdez8xiREiSXNZZzLfjv2WigtLadnJwkJn2ecf-ObA059RbezDuXjV')
         embed = DiscordEmbed(title='Nouveau Commit', description='Un nouveau commit a été effectué', color='ffdd99')
         embed.set_thumbnail(url="https://cdn-icons-png.flaticon.com/512/25/25231.png")
         embed.add_embed_field(name=f"Author",value=f"{auth[i]}")
-        embed.add_embed_field(name="Date",value=f"{up[i]}")
+        embed.add_embed_field(name="Date",value=f"{up[i][:-6]}")
         embed.add_embed_field(name="Message",value=f"{com[i]}",inline=False)
+        embed.add_embed_field(name="Hash",value=f"{hash[i]}")
+        embed.add_embed_field(name="Ish",value=f"{d[i][0]}")
+        embed.add_embed_field(name="Fichiers modifiés par ce commit",value=f"{d[i][1]}",inline=False)
+        embed.set_footer(icon_url="https://cdn-icons-png.flaticon.com/512/25/25231.png",text=f"Modifications sur le GIT '{name}'")
 
 
         webhook.add_embed(embed)
--- commit_tracker.sh
+++ commit_tracker.sh
@@ -3,9 +3,16 @@ do
 git log -10 HEAD --pretty=format:%s >com.txt
 git log -10 --pretty=format:'%an' >author.txt
 git log -10 --format=%cd >date.txt
+git log -10 --format=format:"%H" >hash.txt
+basename -s .git `git config --get remote.origin.url` >name.txt
+git show -10 --name-only --oneline HEAD >sortie.txt
 python3 commit_tracker.py
 rm date.txt
 rm author.txt
 rm com.txt
+rm name.txt
+rm hash.txt
+rm sortie.txt
+rm -f commit.txt
 sleep $1
 done
