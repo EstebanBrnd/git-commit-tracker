a7fbe8a09ba2076793568860caa503efbdfb34eb
Esteban Brnd <106627321+EstebanBrnd@users.noreply.github.com>
2022-12-03 09:06:51 +0100

    Update commit_tracker.sh
    Ajout des commentaires

--- commit_tracker.sh
+++ commit_tracker.sh
@@ -1,18 +1,21 @@
 while true
 do
-git log -10 HEAD --pretty=format:%s >com.txt
-git log -10 --pretty=format:'%an' >author.txt
-git log -10 --format=%cd >date.txt
-git log -10 --format=format:"%H" >hash.txt
-basename -s .git `git config --get remote.origin.url` >name.txt
-git show -10 --name-only --oneline HEAD >sortie.txt
-python3 commit_tracker.py
-rm date.txt
+git fetch
+git pull --quiet                                                  #Les fichiers sont désormais à jour
+git log -10 HEAD --pretty=format:%s >com.txt                      #Récupération des 10 derniers messages de commit
+git log -10 --pretty=format:'%an' >author.txt                     #Récupération des 10 derniers auteurs de commit
+git log -10 --format=%cd >date.txt                                #Récupération des 10 dernieres dates de commit
+git log -10 --format=format:"%H" >hash.txt                        #Récupération des 10 derniers hashs de commit
+basename -s .git `git config --get remote.origin.url` >name.txt   #Récupération des 10 dernieres listes de fichiers modifiés
+git show -10 --name-only --oneline HEAD >sortie.txt               #Récupération des 10 derniers commit-ish de commit
+python3 commit_tracker.py                                         #Appel au fichier python pour envoyer le message
+rm date.txt                                                       #Suppression de tous les fichiers générés par les fonctions précédentes
 rm author.txt
 rm com.txt
 rm name.txt
 rm hash.txt
 rm sortie.txt
-rm -f commit.txt
-sleep $1
+rm -f commit.txt                                                  #Suppression sans message d'erreur car commit.txt est produit dans commit_tracker.py 
+                                                                  #uniquement si il y a eu des modifications
+sleep $1                                                          #Pause avant de rafraichir de nouveau
 done
